f4985a673b5abaa80baebb7f2ba45dc6e10e55bf
shivanidalmia1203 <47073500+shivanidalmia1203@users.noreply.github.com>
2021-10-13 15:44:35 +0530

    Add files via upload

---
+++ src/main/java/com/example/messagingredis/ReceiverApplication.java
@@ -0,0 +1,50 @@
+package com.example.messagingredis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@SpringBootApplication
+public class ReceiverApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReceiverApplication.class);
+
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		SpringApplication.run(ReceiverApplication.class, args);
+
+		while (true) {
+
+		}
+
+	}
+}
